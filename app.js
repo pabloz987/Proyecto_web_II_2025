d05c4584dc2a4a47d421beb19248df6562acb9a2
talitumzi <enriquezuleta001@gmail.com>
2025-11-26 21:11:24 -0400

    Eliminacion del front

--- app.js
+++ app.js
@@ -17,18 +17,7 @@ app.get('/',(req,res)=>{
   console.log("El back funciona");
 });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
